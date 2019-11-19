ea388cc0ea44c77fa4111dcc998ac5b2f2a1c09e
Liya7979 <liyacnan7@gmail.com>
2019-11-19 15:31:16 +0100

    started k-armed bandit problem

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+class Bandit {
+private:
+    int k;
+    double epsilon;
+public:
+    Bandit(int k, double epsilon){
+
+    }
+
+
+
+};
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
